c7f16b951a5327cb9096d4b5ff1b2779c999d343
zhenhappy <q505507538@gmail.com>
2017-12-22 18:12:54 +0800

    fix resize calc bug

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 var assign = Object.assign || require('object.assign')
 export const initRem = options => {
   options = assign({
-    clientWidth : document.documentElement.clientWidth,
     designWidth : 375, // 定义设计图尺寸宽度
     maxWidth    : 500, // 定义最大屏幕尺寸, 为了解决平板字体太大问题
     bodyFontSize: 14 , // 定义文档默认字体大小
@@ -10,7 +9,6 @@ export const initRem = options => {
                        // 也不要小于12px, 虽然用10px也不错, 但是开发环境由于是Chrome,
                        // 小于12像素会有问题, 但是真实设备上是没问题的, 所以根据自己的开发环境决定
   }, options)
-
 
   _setBaseFontSize()
   _setBodyFontSize()
@@ -24,7 +22,8 @@ export const initRem = options => {
 
   // 设置REM基准字号 1rem = 设计图尺寸 / 屏幕dpr / 基准字号
   function _setBaseFontSize () {
-    var rem = options.baseFontSize * (options.clientWidth > options.maxWidth ? options.maxWidth : options.clientWidth) / options.designWidth
+    const { clientWidth } = document.documentElement
+    const rem = options.baseFontSize * (clientWidth > options.maxWidth ? options.maxWidth : clientWidth) / options.designWidth
     document.documentElement.style.fontSize = rem + 'px'
   }
 

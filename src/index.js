eafb6ed34de7c9d8baea9f9753435fd2a603292c
zhenhappy <q505507538@gmail.com>
2018-01-06 00:36:04 +0800

    fix bug

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 export const initRem = options => {
-  options = {...options, ...{
+  options = {...{
     designWidth : 375, // 定义设计图尺寸宽度
     maxWidth    : 500, // 定义最大屏幕尺寸, 为了解决平板字体太大问题
     bodyFontSize: 14 , // 定义文档默认字体大小
@@ -7,7 +7,7 @@ export const initRem = options => {
                        // 设置100便于计算, 设计图是2倍图, 元素尺寸100px, 那么就是(100 / 2 / 10)rem
                        // 也不要小于12px, 虽然用10px也不错, 但是开发环境由于是Chrome,
                        // 小于12像素会有问题, 但是真实设备上是没问题的, 所以根据自己的开发环境决定
-  }}
+  }, ...options}
 
   _setBaseFontSize()
   _setBodyFontSize()

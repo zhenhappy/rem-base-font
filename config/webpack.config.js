b4ebd36e21ed6492d0283d53da6ff9cd89aa7141
zhenhappy <q505507538@gmail.com>
2018-04-13 09:48:56 +0800

    update library name

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
     path: path.resolve(__dirname, "../dist"),
     filename: 'index.js',
     libraryTarget: "umd",
-    library: "init-rem"
+    library: "initRem"
   },
   module: {
     loaders:[
